2a71e52037d020b6d4c116569ecb8138e2b79f41
dong <2453611300@qq.com>
2020-12-22 19:59:26 +0800

    测试fork

--- api.php
+++ api.php
@@ -3,6 +3,7 @@
  * [WeEngine System] Copyright (c) 2014 WE7.CC
  * WeEngine is NOT a free software, it under the license terms, visited http://www.we7.cc/ for more details.
  */
+//顶顶顶顶
 define('IN_API', true);
 require_once './framework/bootstrap.inc.php';
 load()->model('reply');
